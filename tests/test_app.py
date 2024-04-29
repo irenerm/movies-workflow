7de103d08816b2682f40374d05f818170cca5daf
irenerm <100451244@alumnos.uc3m.es>
2024-04-29 12:55:39 +0200

    pylint modifications

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,82 +1,52 @@
+"""Módulo de pruebas para verificar la funcionalidad de la aplicación web."""
+
 import json
 import pathlib
-
 import pytest
 from jsonschema import validate, RefResolver
-
 from web_app import app
 from movies.movie import Movie
 
-
 @pytest.fixture
-def client():
+def test_client():
+    """Configura el cliente de prueba para la aplicación Flask."""
     app.config["TESTING"] = True
-
     with app.test_client() as client:
         yield client
 
-
 def validate_payload(payload, schema_name):
-    """
-    Validate payload with selected schema
-    """
-    schemas_dir = str(
-        f"{pathlib.Path(__file__).parent.absolute()}/schemas"
-    )
-    schema = json.loads(pathlib.Path(f"{schemas_dir}/{schema_name}").read_text())
+    """Valida un payload según el esquema JSON especificado."""
+    schemas_dir = str(pathlib.Path(__file__).parent.absolute() / "schemas")
+    schema_path = pathlib.Path(f"{schemas_dir}/{schema_name}")
+    schema = json.loads(schema_path.read_text(encoding="utf-8"))
     validate(
         payload,
         schema,
-        resolver=RefResolver(
-            "file://" + str(pathlib.Path(f"{schemas_dir}/{schema_name}").absolute()),
-            schema  # it's used to resolve the file inside schemas correctly
-        )
+        resolver=RefResolver("file://" + str(schema_path.absolute()), schema)
     )
 
-
-def test_create_movie(client):
+def test_create_movie(test_client):
+    """Prueba la creación de una película mediante la API."""
     data = {
         "title": "Avatar",
         "duration": 178,
         "category": "Action"
     }
-    response = client.post(
+    response = test_client.post(
         "/create-movie/",
-        data=json.dumps(
-            data
-        ),
+        data=json.dumps(data),
         content_type="application/json",
     )
-
     validate_payload(response.json, "Movie.json")
 
-
-def test_get_movie(client):
-
-    movie = Movie(
-        title = "Avatar",
-        duration = 178,
-        category = "Action"
-    ).save()
-
-    response = client.get(
-        f"/movie/{movie.id}/",
-        content_type="application/json",
-    )
-
+def test_get_movie(test_client):
+    """Prueba la obtención de detalles de una película específica por ID."""
+    movie = Movie(title="Avatar", duration=178, category="Action").save()
+    response = test_client.get(f"/movie/{movie.id}/", content_type="application/json")
     validate_payload(response.json, "Movie.json")
 
-
-def test_list_movies(client):
-
-    Movie(
-        title = "Avatar",
-        duration = 178,
-        category = "Action"
-    ).save()
-    response = client.get(
-        "/movie-list/",
-        content_type="application/json",
-    )
-
+def test_list_movies(test_client):
+    """Prueba la recuperación de una lista de películas."""
+    Movie(title="Avatar", duration=178, category="Action").save()
+    response = test_client.get("/movie-list/", content_type="application/json")
     validate_payload(response.json, "MovieList.json")
