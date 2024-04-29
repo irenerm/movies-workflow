3db300cd51690bb17e00c54c9d2dce31a9e3ff2c
irenerm <100451244@alumnos.uc3m.es>
2024-04-29 12:26:36 +0200

    pylint modifications

--- movies/movie_commands.py
+++ movies/movie_commands.py
@@ -1,13 +1,27 @@
-from pydantic import BaseModel
 from typing import List
+from pydantic import BaseModel
 from movies.movie import Movie
 
 
 class CreateMovieCommand(BaseModel):
+    """
+    Comando para crear una nueva película si no existe.
+    
+    Attributes:
+        title (str): El título de la película.
+        duration (int): La duración de la película en minutos.
+        category (str): La categoría de la película.
+    """
     title: str
     duration: int
     category: str
 
+    """
+    Ejecuta el comando para crear una nueva película o devolver una existente basada en el título.
+        
+    Returns:
+        Movie: La película creada o encontrada.
+    """
     def execute(self) -> Movie:
         movie = Movie.get_by_title(self.title)
         if movie is None:
@@ -20,6 +34,9 @@ class CreateMovieCommand(BaseModel):
 
 
 class ListMovies(BaseModel):
+    """
+    Comando para listar todas las películas disponibles.
+    """
 
     def execute(self) -> List[Movie]:
         movies = Movie.list()
@@ -27,8 +44,20 @@ class ListMovies(BaseModel):
 
 
 class GetMovieById(BaseModel):
+    """
+    Comando para obtener una película por su identificador.
+    
+    Attributes:
+        id (str): El identificador único de la película.
+    """
     id: str
 
     def execute(self) -> Movie:
+        """
+        Ejecuta el comando para obtener una película basada en su ID.
+        
+        Returns:
+            Movie: La película obtenida.
+        """
         movie = Movie.get_by_id(self.id)
         return movie
