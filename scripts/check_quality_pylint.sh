72b36299bf59441893fd2fbf1d33a1a724fc805d
irenerm <100451244@alumnos.uc3m.es>
2024-04-29 18:20:43 +0200

    modifications in pylint script

--- scripts/check_quality_pylint.sh
+++ scripts/check_quality_pylint.sh
@@ -3,15 +3,17 @@
 # Umbral mínimo de calificación para pylint
 MIN_SCORE=7.0
 
-# Ejecuta pylint en todos los archivos Python y almacena el resultado
-PYLINT_OUTPUT=$(find . -name "*.py" -exec pylint --fail-under=$MIN_SCORE {} +)
+# Ejecuta pylint solo en los archivos Python dentro de la carpeta 'movies'
+# y almacena el resultado
+PYLINT_OUTPUT=$(find movies -name "*.py" -exec pylint --fail-under=$MIN_SCORE {} +)
 RESULT=$?
 
 # Verifica el resultado de pylint
 if [ $RESULT -eq 0 ]; then
     echo "Pylint pasó con una calificación igual o superior a $MIN_SCORE"
+    exit 0  # Exit code para indicar éxito
 else
     echo "Pylint falló con una calificación menor a $MIN_SCORE"
     echo "$PYLINT_OUTPUT"
-    exit 1
+    exit 1  # Exit code para indicar fallo
 fi
